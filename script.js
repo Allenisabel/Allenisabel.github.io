983009cbf8183043604a8c674c6be717961411e6
猫鼬君 <304819045@qq.com>
2025-11-21 00:20:46 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -325,4 +325,20 @@ function initGame() {
                 <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
                 <circle cx="12" cy="7" r="4"></circle>
             </svg>
+            人人对战
+        `;
+    } else {
+        aiModeBtn.classList.remove('active');
+        aiControls.classList.add('hidden');
+        aiModeBtn.innerHTML = `
+            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm0 18a8 8 0 1 1 8-8 8 8 0 0 1-8 8z"/>
+                <path d="M12 6v6l4 2"/>
+            </svg>
+            人机对战
+        `;
+    }
+    restartGame();
+}
+
 initGame();
